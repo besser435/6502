4f5662c0a76d33c8ac74d92c3d4a4619b641ea78
Brandon <68145204+besser435@users.noreply.github.com>
2022-12-13 04:30:31 -0700

    fibonacci

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,44 @@
+"""
+This is a Python prototype of the Fibonacci sequence.
+I am dumb and dont know assembly, so I'm using this as a guide
+before I program it for the 6502.
+"""
+
+
+import time
+
+"""while True:
+    fib_1 = 0
+    fib_2 = 0
+
+    fib_num = None
+
+    for i in range(100):
+        fib_num = fib_1 + fib_2
+        fib_1 = fib_2
+        fib_2 = fib_num
+
+
+        print(fib_num)
+        time.sleep(0.2)
+
+    time.sleep(0.2)"""
+
+
+
+while True:
+    fib_1 = 0
+    fib_2 = 0
+
+    fib_num = None
+
+    for i in range(100):
+        fib_num = fib_1 + fib_2
+        fib_1 = fib_2
+        fib_2 = fib_num
+
+
+        print(fib_num)
+        time.sleep(0.2)
+
+    time.sleep(0.2)
