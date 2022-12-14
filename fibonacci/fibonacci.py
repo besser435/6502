95a09ecbb496b86d02dd6944e05203bad7d07871
Brandon <68145204+besser435@users.noreply.github.com>
2022-12-14 06:23:10 -0700

    update

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -3,42 +3,15 @@ This is a Python prototype of the Fibonacci sequence.
 I am dumb and dont know assembly, so I'm using this as a guide
 before I program it for the 6502.
 """
-
-
 import time
 
-"""while True:
-    fib_1 = 0
-    fib_2 = 0
-
-    fib_num = None
-
-    for i in range(100):
-        fib_num = fib_1 + fib_2
-        fib_1 = fib_2
-        fib_2 = fib_num
-
-
-        print(fib_num)
-        time.sleep(0.2)
-
-    time.sleep(0.2)"""
-
-
+n1 = 0
+n2 = 1
 
 while True:
-    fib_1 = 0
-    fib_2 = 0
-
-    fib_num = None
-
-    for i in range(100):
-        fib_num = fib_1 + fib_2
-        fib_1 = fib_2
-        fib_2 = fib_num
-
-
-        print(fib_num)
-        time.sleep(0.2)
-
-    time.sleep(0.2)
+    fib_number = n1 + n2
+    n1 = n2
+    n2 = fib_number
+    print(fib_number)
+    time.sleep(0.1)
+    
